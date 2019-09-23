869350e8d924ba55e15979fb9e9a72dae16c9938
bmshamsnahid <b.m.shamsnahid@gmail.com>
2019-09-24 01:54:02 +0600

    add git ignore directory

---
+++ contracts/Inbox.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.4.17;
+
+contract Inbox {
+    string public message;
+    
+    function Inbox(string initialMessae) public {
+        message = initialMessae;
+    }
+    
+    function setMessage(string newMessage) public {
+        message = newMessage;
+    }
+} 
---
+++ deploy.js
@@ -0,0 +1,28 @@
+// https://rinkeby.etherscan.io/tx/0x52b83e837ff916cddd38c519deda3c1bfeda0fe07afe99b371f42f0e58d7a5b2
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+  'tube middle glare chimney poet give ship vicious kiss elevator dose silk',
+  'https://www.rinkeby.infura.io/v3/c04f7dc3b4094ec3ad5dace989407d87'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+  console.log('Attempting to deploy from account ', accounts[0]);
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: bytecode,
+      arguments: ['Hi there!']
+    })
+    .send({
+      gas: '1000000',
+      from: accounts[0]
+    });
+  console.log('Contract deployed to ', result.options.address);
+};
+
+deploy();
