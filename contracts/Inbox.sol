36f3e36222719207b45cd2358f72ffcc6e6ae559
root <root@Nahid.ixora.com>
2019-09-24 12:01:53 +0600

    add package-lock.json in gitignore

--- contracts/Inbox.sol
+++ contracts/Inbox.sol
@@ -2,12 +2,12 @@ pragma solidity ^0.4.17;
 
 contract Inbox {
     string public message;
-    
+
     function Inbox(string initialMessae) public {
         message = initialMessae;
     }
-    
+
     function setMessage(string newMessage) public {
         message = newMessage;
     }
-} 
+}
